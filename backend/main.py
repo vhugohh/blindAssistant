a5e9cd33bcbe49db3566f83ca225f96b29edb988
vhugohh <vhugohh@hotmail.com>
2026-07-07 13:36:40 -0600

    Add endpoints for object recognition and weather analysis
    Se añadieron las opciones para reconocimiento y descripción de objetos y para la indicación del clima.

--- backend/main.py
+++ backend/main.py
@@ -130,7 +130,53 @@ valor total y moneda.
     return {
         "result": texto
     }
+
+# =====================================================
+# RECONOCER OBJETOS
+# =====================================================
+
+@app.post("/object")
+
+def money(data: ImageRequest):
+
+    prompt = """
+Describe solamente el objeto que se encuentra en la imagen en primer plano.
+
+Usa máximo 50 palabras
+
+"""
+
+    texto = analizar(
+        data.image,
+        prompt
+    )
+
+    return {
+        "result": texto
+    }
+
+# =====================================================
+# CLIMA
+# =====================================================
+
+@app.post("/weather")
+
+def money(data: ImageRequest):
+
+    prompt = """
+Indica la fecha y hora. Menciona las condiciones meteorológicas actuales y el pronóstico del clima.
+"""
+
+    texto = analizar(
+        data.image,
+        prompt
+    )
+
+    return {
+        "result": texto
+    }
     
+
 # =====================================================
 # FRONTEND
 # =====================================================
